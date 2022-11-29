01aa15538a43faa7410252f0c6e0c644e8e7dc16
Adam Diehl <adadiehl@umich.edu>
2022-11-29 11:12:23 -0500

    First commit

--- bootstrap_server.sh
+++ bootstrap_server.sh
@@ -21,10 +21,31 @@ else
     echo "Cron daemon is running.";
 fi
 
+echo "Checking /tmp for example data and results..."
+if [ ! -f /tmp/example_data/example_data.tar.gz ]; then
+    echo "Example data not found.";
+    if [ ! -d /tmp/example_data ]; then
+	echo "Creating /tmp/example_data";
+	mkdir /tmp/example_data/;
+    fi
+    echo "Copying example_data.tar.gz to /tmp/example_data.";
+    cp /home/node/bulk_plasmid_seq_web/example_data/example_data.tar.gz /tmp/example_data/;
+fi
+
+if [ ! -d /tmp/example_results/ ]; then
+    echo "Example results not found.";
+    echo "Copying example_results.tar.gz to /tmp.";
+    cp /home/node/bulk_plasmid_seq_web/example_data/example_results.tar.gz /tmp;
+    cd /tmp;
+    echo "Unpacking example_results.tar.gz.";
+    tar -xzf example_results.tar.gz;
+    rm example_results.tar.gz;
+fi
+
 echo "Starting the bulk_plasid_seq_web app..."
 #cd /home/node/bulk_plasmid_seq_web && conda activate medaka && npm start 1>&2 2> /var/log/bulk_plasmid_seq_web.log
 # pm2 daemonizes server.js
 conda activate medaka
-cd /home/node/bulk_plasmid_seq_web/backend && pm2 start server.js --log /var/log/bulk_plasmid_seq_web.log --time
+cd /home/node/bulk_plasmid_seq_web/backend && pm2 start server.js -l /var/log/bulk_plasmid_seq_web.log -e /var/log/bulk_plasmid_seq_web.log -o /var/log/bulk_plasmid_seq_web.log --time
 cd /home/node/bulk_plasmid_seq_web/client && pm2 start "npm start"
 pm2 save
